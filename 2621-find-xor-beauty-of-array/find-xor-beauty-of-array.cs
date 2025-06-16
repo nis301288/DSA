4e6f914f7957026be2531c8acd6c9abd47c4392a
nis301288 <106837131+nis301288@users.noreply.github.com>
2025-06-16 16:34:48 +0530

    Update find-xor-beauty-of-array.cs

--- 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.cs
+++ 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int XorBeauty(int[] nums) {
-       //Optimized(XOR) approach
+       //Optimized(XOR) approach(2527. Find Xor-Beauty of Array)
        int n = nums.Length;
        int result = 0;
        for(int i = 0; i<n; i++)
