08ff6d27f60f3ab24f88ed8fefbe2ae9f68bd294
nis301288 <106837131+nis301288@users.noreply.github.com>
2025-06-24 15:55:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (77.59%) - LeetSync

---
+++ 66-plus-one/plus-one.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        
+  //Array Problem Statement
+        int n = digits.Length;
+         /* n-1 because array is running from right to left*/
+        for(int i = n-1; i>=0; i--)
+        {
+       
+       if(digits[i]==9)
+       {
+       
+       digits[i]=0;
+
+       }
+       else{
+
+        digits[i]++;
+        return digits;
+       }
+
+        }
+      /* if all are 9 in array then will convert into 0 and create new array and include 1.[9,9,9,9]->[0,0,0,0]->[1,0,0,0,0] */
+        int[] result = new int[n+1];
+        result[0]=1;
+        return result; 
+    }
+}
